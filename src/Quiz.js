4562b608b428a90cfcd12d9c74969347afdeb6e8
Jack Buckley <jack.buckley@wrenkitchens.com>
2020-10-05 12:05:25 +0100

    hb

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
+import "./App.css";
 
-let quizData = require("./quiz_data.json");
+import Quiz from "./Quiz.js";
 
-class Quiz extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { quiz_position: 1 };
-  }
+class App extends Component {
   render() {
-    <div> Quiz</div>;
+    return (<div />), (<Quiz />);
   }
 }
-
-export default Quiz;
