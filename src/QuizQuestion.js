7644b54ac9c6445d43be00dc63555dc3f0529283
Jack Buckley <jack.buckley@wrenkitchens.com>
2020-10-05 12:58:18 +0100

    Module 2 Commit 5

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import Quiz from "./Quiz";
+import Quiz from "./Quiz.js";
 
 class QuizQuestion extends Component() {
   render() {
     <main>
       <section>
-        <p>// instruction text goes here</p>
+        <p> {this.props.quiz_question.instruction_text}</p>
       </section>
       <section className="buttons">
         <ul>
