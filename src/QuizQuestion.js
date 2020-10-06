a882ce3395c0273928e51e2b1c116442b408b162
Jack Buckley <jack.buckley@wrenkitchens.com>
2020-10-06 15:28:22 +0100

    h

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -12,12 +12,21 @@ class QuizQuestion extends Component {
           <ul>
             <QuizQuestionButton
               button_text={this.props.quiz_question.answer_options[0]}
-            />
+              {this.props.quiz_question.answer_options.map(
+                (answer_option, index) => (
+                  <div>
+                    <QuizQuestionButton clickHandler ={clickHandler.bind(this)}
+                    key={index}
+                    button_text = {answer_option}
+                
+
+                />
+              
+          </div>
+                )
+              )}
           </ul>
         </section>
       </main>
-    );
-  }
-}
 
 export default QuizQuestion;
