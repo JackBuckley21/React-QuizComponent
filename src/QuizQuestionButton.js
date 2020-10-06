44a90f43b72bc5826a5ab8aaf221424af8b2b2de
Jack Buckley <jack.buckley21@gmail.com>
2020-10-06 21:12:30 +0100

    Module 5 Final Push

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component {
   handleClick() {
@@ -7,10 +7,10 @@ class QuizQuestionButton extends Component {
   render() {
     return (
       <li>
-        <button onCLick={this.handleClick.bind(this)}>{this.props.button_text}</button>
+        <button onClick={this.handleClick.bind(this)}>{this.props.button_text}</button>
       </li>
-    );
+    )
   }
 }
 
-export default QuizQuestionButton;
+export default QuizQuestionButton 
