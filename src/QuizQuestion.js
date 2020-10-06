3dcd3489f940d14f072f77fc013dab313bd2cf78
Jack Buckley <jack.buckley@wrenkitchens.com>
2020-10-06 14:44:54 +0100

    Test?

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,21 +1,14 @@
 import React, { Component } from "react";
-import Quiz from "./Quiz.js";
 
 class QuizQuestion extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
   render() {
     return (
       <main>
         <section>
-          {" "}
           <p> {this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
-            {" "}
             <li>{this.props.quiz_question.answer_options[0]}</li>
           </ul>
         </section>
