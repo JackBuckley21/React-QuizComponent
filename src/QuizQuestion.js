58c1981384edfd22e5bf4e782a48fb3d0c0ebe2a
Jack Buckley <jack.buckley21@gmail.com>
2020-10-06 20:50:17 +0100

    TEST

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
-import QuizQuestionButton from "./QuizQuestionButton";
-
+import QuizQuestionButton from "./QuizQuestionButton.js";
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleClick(buttonText) {
+    buttonText === this.props.quiz_question.answer
+      ? this.props.showNextQuestionHandler()
+      : "";
+  }
+
   render() {
     return (
       <main>
@@ -12,25 +22,22 @@ class QuizQuestion extends Component {
           <ul>
             <QuizQuestionButton
               button_text={this.props.quiz_question.answer_options[0]}
-              />
-              {this.props.quiz_question.answer_options.map(
-                (answer_option, index) => (
-                  <div>
-                    <QuizQuestionButton clickHandler ={clickHandler.bind(this)}
+            />
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => (
+                <div>
+                  <QuizQuestionButton
+                    clickHandler={this.handleClick.bind(this)}
                     key={index}
-                    button_text = {answer_option}
-                
-
-                />
-              
-          </div>
-                )
-              )}
+                    button_text={answer_option}
+                  />
+                </div>
+              )
+            )}
           </ul>
         </section>
       </main>
     );
-                }
-              }
-
+  }
+}
 export default QuizQuestion;
